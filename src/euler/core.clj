3c6f33d8ef62dfb65b6190d63686ff756f961c93
Bobby Eickhoff <bobby.eickhoff2@kroger.com>
2014-02-17 13:00:16 -0500

    An hour-and-a-half later

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,7 +1,30 @@
 (ns euler.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn divides? [product factor]
+  (zero? (mod product factor)))
+;
+;(defn prime? [x]
+;    (contains? (buildPrimes (sqrt x), []) x)
+;)
+
+(defn buildPrimes  [max, knownPrimes, current]
+  (cond
+    (= current max)
+      knownPrimes
+    (isNextPrime knownPrimes current)
+      (recur max (conj knownPrimes current) (+ current 1))
+    :else
+      (recur max knownPrimes (+ current 1))))
+
+(defn isNextPrime [primes candidate]
+  (not (some (partial divides? candidate) primes)))
+
+
+(defn get-factors [testNumber primes]
+  (filter (partial divides? testNumber) primes)
+  )
+
+( defn get-largest-prime-factor [testNumber]
+  (last (get-factors testNumber (buildPrimes testNumber #{} 2)))
+  )
--- test/euler/core_test.clj
+++ test/euler/core_test.clj
@@ -2,5 +2,32 @@
   (:require [midje.sweet :refer :all]
             [euler.core :refer :all]))
 
-(fact "FIXME, I fail."
-  0 => 1)
+;(fact "a composite number should not be considered prime"
+;  (prime? 2) => true
+;  (prime? 4) => false)
+
+(fact "a factor divides its multiples"
+  (divides? 6 3) => true
+  (divides? 7 3) => false)
+
+(fact "buildPrimes returns a set of prime numbers"
+  (buildPrimes 10 #{} 2) => #{2 3 5 7})
+
+(fact "isNextPrime return the true if next number is prime"
+  (isNextPrime #{2 3 5 7} 11 ) => true
+  )
+
+(fact "isNextPrime returns false if next number is not prime"
+  (isNextPrime #{2 3 5 7} 10 ) => false
+  (isNextPrime #{2 3 5 7} 12 ) => false
+  )
+
+(fact "get prime factors"
+  (get-factors 15 #{2 3 5 7 11 13}) => '(3 5))
+
+(fact "get largest prime factor"
+  (get-largest-prime-factor 13195)=> 29
+  )
+
+(fact "solve euler 3"
+  (get-largest-prime-factor 600851475143) => 6857)
